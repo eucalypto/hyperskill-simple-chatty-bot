1674336d7ebc0dd642c80de90295262f8a83463a
Viktor Papara <eucalypto@mail.de>
2020-12-15 15:33:44 +0100

    Add: reading name and printing message (#2)
    closes #2

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,8 +1,17 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! My name is TARS.");
         System.out.println("I was created in 2020.");
+        System.out.println("Please, remind me your name.");
+
+        var userName = scanner.next();
+
+        System.out.printf("What a great name you have, %s!%n", userName);
     }
 }
