7fbf640095045a52296fa485a705d5b1a2c429e0
Viktor Papara <eucalypto@mail.de>
2020-12-15 15:45:50 +0100

    Add starting files for stage 4/5 (#4)

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -6,8 +6,8 @@ public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! My name is TARS.");
-        System.out.println("I was created in 2020.");
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
         String name = scanner.nextLine();
@@ -16,12 +16,17 @@ public class SimpleBot {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        var remainder3 = scanner.nextInt();
-        var remainder5 = scanner.nextInt();
-        var remainder7 = scanner.nextInt();
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
 
-        var age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
 
-        System.out.printf("Your age is %d; that's a good time to start programming!", age);
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        // read a number and count to it here
+
+        System.out.println("Completed, have a nice day!");
     }
 }
